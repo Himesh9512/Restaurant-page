69e78be787856ac9d1ece8950bb9e1c703153205
Himesh9512 <himeshjangid0000@gmail.com>
2023-01-03 17:31:27 +0530

    Configure css loaders in webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import './style.css';
+
 function helloWorld(){
+    const content = document.getElementById('content');
+    const testElement = document.createElement('div');
+    testElement.classList.add('test');
+    testElement.innerText = "Hello guys this is under construction website";
+    content.appendChild(testElement);
     console.log("hello world");
 }
 
