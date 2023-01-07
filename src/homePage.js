f3a1efa55578c371571144892d19e3deb8447333
Himesh9512 <himeshjangid0000@gmail.com>
2023-01-07 21:14:23 +0530

    Add menu button to home page

--- src/homePage.js
+++ src/homePage.js
@@ -1,15 +1,29 @@
 const addMainText = () => {
     const mainContentText = document.createElement('div');
     mainContentText.classList.add('content-text');
-
     mainContentText.innerHTML = 'Fake Cookies & <span>Smiles</span>';
 
     return mainContentText;
 }
 
+const createMenuButton = () => {
+    const menuButton = document.createElement('div');
+    menuButton.classList.add('menu-button');
+
+    const button = document.createElement('button');
+    button.innerText = 'Menu';
+
+    button.addEventListener('click',()=>{
+        console.log('menu is here');
+    })
+    menuButton.appendChild(button);
+
+    return menuButton;
+}
 export default function loadHomePage () {
     const main = document.getElementById('main');
-
+    
     main.innerHTML = '';
     main.appendChild(addMainText());
+    main.appendChild(createMenuButton());
 }
