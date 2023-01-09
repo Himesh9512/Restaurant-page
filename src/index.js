804f0fe04b3508e1cd8021fc323226fed8daa46f
Himesh9512 <himeshjangid0000@gmail.com>
2023-01-09 13:18:27 +0530

    Add footer to base webpage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import logoImage from './assets/images/logo.png';
+import githubLogo from './assets/images/github.png';
 import { createNavigationButton, createImageElement } from './utilities';
 import './style.css';
 import loadHomePage from './homePage';
@@ -35,11 +36,29 @@ const createMainPage = () => {
     return mainPage;
 }
 
+const createFooter = () => {
+    const footer = document.createElement('div');
+    footer.setAttribute('id','footer');
+
+    const footerContent = document.createElement('div');
+    footerContent.innerText = 'Copyright © Himesh9512';
+
+    const footerIcon = document.createElement('a');
+    footerIcon.href = 'https://github.com/Himesh9512/';
+    footerIcon.appendChild(createImageElement(githubLogo,'github','github-logo'));
+    
+    footer.appendChild(footerContent);
+    footer.appendChild(footerIcon);
+
+    return footer;
+}
+
 (function heroPage() {
     const content = document.getElementById('content');
 
     content.appendChild(createHeader());
     content.appendChild(createMainPage());
+    content.appendChild(createFooter());
 
     loadHomePage();
 })();
